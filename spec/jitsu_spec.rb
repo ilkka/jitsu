928d3aa2f87b487d64826e6525e8b2ccf2a6889c
Ilkka Laukkanen <ext-ilkka.laukkanen@nokia.com>
2011-02-10 15:22:52 +0200

    First specs for locating ninja

--- spec/jitsu_spec.rb
+++ spec/jitsu_spec.rb
@@ -1,7 +1,14 @@
 require File.expand_path(File.dirname(__FILE__) + '/spec_helper')
 
 describe "Jitsu" do
-  it "fails" do
-    fail "hey buddy, you should probably rename this file and start specing for real"
+  it "finds ninja if ninja is in PATH" do
+    File.executable?(Jitsu.ninja).should be_true
+  end
+
+  it "fails to find ninja if ninja is not in PATH" do
+    oldpath = ENV['PATH']
+    ENV['PATH'] = ""
+    Jitsu.ninja.should == nil
+    ENV['PATH'] = oldpath
   end
 end
