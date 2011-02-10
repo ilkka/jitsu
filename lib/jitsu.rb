d0baaf184ca303d7319f565cae2868a85db5cd37
Ilkka Laukkanen <ilkka.s.laukkanen@gmail.com>
2011-02-10 21:20:02 +0200

    First Yard comment, woo

--- lib/jitsu.rb
+++ lib/jitsu.rb
@@ -1,4 +1,7 @@
 module Jitsu
+  # Get path to ninja.
+  #
+  # @return [String] path to `ninja` or nil if ninja was not found.
   def self.ninja
     candidates = ENV['PATH'].split(/:/).map { |d| File.join d, 'ninja' }
     candidates.select { |n| File.executable? n }.first
