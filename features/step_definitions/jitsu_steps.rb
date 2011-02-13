86aa8a3ecbcc4b584ce680c4fcf3152b1e11650e
Ilkka Laukkanen <ilkka.s.laukkanen@gmail.com>
2011-02-13 17:30:47 +0200

    Skeleton step definitions file

---
+++ features/step_definitions/jitsu_steps.rb
@@ -0,0 +1,16 @@
+Given /^a file "([^"]*)" with contents$/ do |arg1, string|
+    pending # express the regexp above with the code you wish you had
+end
+
+When /^I run jitsu$/ do
+    pending # express the regexp above with the code you wish you had
+end
+
+When /^I run "([^"]*)"$/ do |arg1|
+    pending # express the regexp above with the code you wish you had
+end
+
+Then /^the output should be "([^"]*)"$/ do |arg1|
+    pending # express the regexp above with the code you wish you had
+end
+
