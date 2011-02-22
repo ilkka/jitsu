632f413e0d575e75a11a092f03ac283c0b46dc66
Ilkka Laukkanen <ilkka.s.laukkanen@gmail.com>
2011-02-22 15:26:05 +0200

    Move target creation to before hook

--- spec/target_spec.rb
+++ spec/target_spec.rb
@@ -1,25 +1,29 @@
 require File.expand_path(File.dirname(__FILE__) + '/spec_helper')
 
 describe "Target" do
-  it "takes attributes from constructor arguments" do
-    Jitsu::Target.public_instance_methods.should include :name
-    t = Jitsu::Target.new(:name => "foo.o",
+
+  before :each do
+    @t = Jitsu::Target.new(:name => "foo.o",
                           :type => "object",
                           :source => "foo.cpp",
                           :cxxflags => "-Wall -ansi -pedantic")
-    t2 = Jitsu::Target.new(:name => "foo",
+    @t2 = Jitsu::Target.new(:name => "foo",
                            :type => "executable",
-                           :objects => [t],
+                           :objects => [@t],
                            :ldflags => "-lbar")
-    t.name.should == "foo.o"
-    t.type.should == "object"
-    t.source.should == "foo.cpp"
-    t.cxxflags.should == "-Wall -ansi -pedantic"
-    t.ldflags.should be_nil
-    t2.name.should == "foo"
-    t2.type.should == "executable"
-    t2.objects.should == [t]
-    t2.source.should be_nil
-    t2.ldflags.should == "-lbar"
+  end
+
+  it "takes attributes from constructor arguments" do
+    Jitsu::Target.public_instance_methods.should include :name
+    @t.name.should == "foo.o"
+    @t.type.should == "object"
+    @t.source.should == "foo.cpp"
+    @t.cxxflags.should == "-Wall -ansi -pedantic"
+    @t.ldflags.should be_nil
+    @t2.name.should == "foo"
+    @t2.type.should == "executable"
+    @t2.objects.should == [@t]
+    @t2.source.should be_nil
+    @t2.ldflags.should == "-lbar"
   end
 end
