144210400e203aa6b4a412aad478af16b41ee9da
Ilkka Laukkanen <ilkka.s.laukkanen@gmail.com>
2011-02-22 15:10:47 +0200

    More comprehensive spec for target featuring interlinking

--- spec/target_spec.rb
+++ spec/target_spec.rb
@@ -3,7 +3,21 @@ require File.expand_path(File.dirname(__FILE__) + '/spec_helper')
 describe "Target" do
   it "has a name" do
     Jitsu::Target.public_instance_methods.should include :name
-    Jitsu::Target.new(:name => "my special target").name.
-      should == "my special target"
+    t = Jitsu::Target.new(:name => "foo.o",
+                          :type => "object",
+                          :source => "foo.cpp",
+                          :cxxflags => "-Wall -ansi -pedantic")
+    t2 = Jitsu::Target.new(:name => "foo",
+                           :type => "executable",
+                           :objects => [t],
+                           :ldflags => "-lbar")
+    t.name.should == "foo.o"
+    t.type.should == "object"
+    t.source.should == "foo.cpp"
+    t.cxxflags.should == "-Wall -ansi -pedantic"
+    t2.name.should == "foo"
+    t2.type.should == "executable"
+    t2.objects.should == [t]
+    t2.ldflags.should == "-lbar"
   end
 end
