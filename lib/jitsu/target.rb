77f0259da6bc56c87b31a7d996b8c83813ce8a3e
Ilkka Laukkanen <ilkka.s.laukkanen@gmail.com>
2011-02-22 14:46:13 +0200

    Target class that takes name attribute from conf

---
+++ lib/jitsu/target.rb
@@ -0,0 +1,19 @@
+module Jitsu
+
+  # Target represents a buildable target.
+  #
+  class Target
+
+    attr_accessor :name
+
+    # Create a new Target instance.
+    #
+    # @param conf [Hash] target configuration as read from the
+    # jitsufile.
+    def initialize(conf)
+      self.name = conf['name']
+    end
+
+  end
+
+end
