d14acd1358a5886bdacaa0b4c5b291a280bbed97
Ilkka Laukkanen <ilkka.s.laukkanen@gmail.com>
2011-02-22 15:33:35 +0200

    Rework target spec into smaller pieces

--- spec/target_spec.rb
+++ spec/target_spec.rb
@@ -13,17 +13,41 @@ describe "Target" do
                            :ldflags => "-lbar")
   end
 
-  it "takes attributes from constructor arguments" do
+  it "has a name attribute" do
     Jitsu::Target.public_instance_methods.should include :name
+    Jitsu::Target.public_instance_methods.should include :name=
     @t.name.should == "foo.o"
-    @t.type.should == "object"
-    @t.source.should == "foo.cpp"
-    @t.cxxflags.should == "-Wall -ansi -pedantic"
-    @t.ldflags.should be_nil
     @t2.name.should == "foo"
+  end
+
+  it "has a type attribute" do
+    Jitsu::Target.public_instance_methods.should include :type
+    Jitsu::Target.public_instance_methods.should include :type=
+    @t.type.should == "object"
     @t2.type.should == "executable"
-    @t2.objects.should == [@t]
+  end
+
+  it "may have a source attribute if it is an object" do
+    Jitsu::Target.public_instance_methods.should include :source
+    Jitsu::Target.public_instance_methods.should include :source=
+    @t.source.should == "foo.cpp"
     @t2.source.should be_nil
+  end
+
+  it "may have an objects attribute if it is a linked target" do
+    Jitsu::Target.public_instance_methods.should include :objects
+    Jitsu::Target.public_instance_methods.should include :objects=
+    @t.objects.should == nil
+    @t2.objects.should == [@t]
+  end
+
+  it "may have ldflags if it's a linked target" do
+    @t.ldflags.should be_nil
     @t2.ldflags.should == "-lbar"
+  end
+
+  it "may have cxxflags if it's an object" do
+    @t.cxxflags.should == "-Wall -ansi -pedantic"
+    @t2.cxxflags.should be_nil
   end
 end
