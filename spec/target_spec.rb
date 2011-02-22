4607970632fa9f1c8c05fdca3a14286421d402eb
Ilkka Laukkanen <ilkka.s.laukkanen@gmail.com>
2011-02-22 15:01:24 +0200

    Use sym key in target spec constructor call

--- spec/target_spec.rb
+++ spec/target_spec.rb
@@ -3,7 +3,7 @@ require File.expand_path(File.dirname(__FILE__) + '/spec_helper')
 describe "Target" do
   it "has a name" do
     Jitsu::Target.public_instance_methods.should include :name
-    Jitsu::Target.new("name" => "my special target").name.
+    Jitsu::Target.new(:name => "my special target").name.
       should == "my special target"
   end
 end
