a3a2fbb28507591d957164cd32dcbebed9a9fcf5
Ilkka Laukkanen <ilkka.s.laukkanen@gmail.com>
2011-02-22 13:15:26 +0200

    Check *instance* methods of Target for :name
    Used to check class methods

--- spec/target_spec.rb
+++ spec/target_spec.rb
@@ -2,6 +2,8 @@ require File.expand_path(File.dirname(__FILE__) + '/spec_helper')
 
 describe "Target" do
   it "has a name" do
-    Jitsu::Target.should respond_to :name
+    Jitsu::Target.public_instance_methods.should include :name
+    Jitsu::Target.new(:name => "my special target").name.
+      should == "my special target"
   end
 end
