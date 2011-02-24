7afa0a5582566e46c9514191ac91d45a3ea5dc9c
Ilkka Laukkanen <ilkka.s.laukkanen@gmail.com>
2011-02-24 15:34:56 +0200

    Targets should store other data too like cxxflags

--- spec/target_spec.rb
+++ spec/target_spec.rb
@@ -4,4 +4,13 @@ describe Jitsu::Target do
   it "has inputs and outputs" do
     [:inputs, :outputs].each {|x| Jitsu::Target.public_instance_methods.should include x}
   end
+
+  it "can have other arbitrary data attached" do
+    Flags1 = "-Wall -ansi -pedantic"
+    t = Jitsu::Target.new :cxxflags => Flags1
+    t[:cxxflags].should == Flags1
+    Flags2 = "-O0 -g"
+    t[:cxxflags] = Flags2
+    t[:cxxflags].should == Flags2
+  end
 end
